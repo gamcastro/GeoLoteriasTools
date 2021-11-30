e21098ff309ed4960e4e2d9426aaea6d943af492
George Castro <gamcastro14@gmail.com>
2021-11-30 12:14:07 -0300

    Implementado menu com multiplas janelas.

--- MenuLoterias.ps1
+++ MenuLoterias.ps1
@@ -1,11 +1,13 @@
 ﻿function Invoke-Menu{
-    
+    Import-Module GeoHomeTools
+    Resize-Console -Maximize
     Clear-Host
-    $host.UI.RawUI.WindowTitle = "GEO LOTERIAS"
+  
+    $host.UI.RawUI.WindowTitle = "Geo Loterias v. 0.0.1"
 
    $title = "Geo Loterias"
 
-   $menuwidth = 30
+   $menuwidth = 103
    [int]$pad = ($menuwidth/2)+($title.length/2)
    $menu = @"
 
@@ -19,8 +21,10 @@
 selecione uma opcao do menu
 
 "@
-
-Write-Host ($title.PadLeft($pad)) -ForegroundColor Cyan
+$dataAtual = (Get-Date).ToString()
+Write-Host "======================================================================================================="
+Write-Host ($title.PadLeft($pad) + $dataAtual.PadLeft(40)) -ForegroundColor Yellow
+Write-Host "======================================================================================================="
 Write-Host $menu -ForegroundColor Yellow
 
 [int]$r=Read-Host "Selecione uma opcao do menu"
@@ -32,9 +36,9 @@ if((1..5) -notcontains $r){
 }
 
 switch ($r) {
-    1 { Invoke-SubMenuMegaSena }
-    2 {Invoke-SubMenuLotofacil}
-    3{}
+    1 { Start-Process powershell -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowMegaSena) -Wait }
+    2 {Start-Process powershell -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowLotofacil) -Wait}
+    3{Start-Process powershell -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowQuina) -Wait}
     5 {
         Clear-Host
         Write-Host "Obrigado" -ForegroundColor Green
@@ -43,7 +47,7 @@ switch ($r) {
 }
 }
 
-function Invoke-SubMenuMegaSena {
+$showWindowMegaSena = {
     Clear-Host
     Write-Host '==================== MegaSena ===================' -ForegroundColor Green
     Write-Host ''
@@ -61,17 +65,16 @@ function Invoke-SubMenuMegaSena {
 
     for ($i = 0;$i -lt $jogos;$i++){
        
-        $aposta = Get-ApostaMegaSena -numeros $numeros |Select-Object -ExpandProperty Aposta
+        $aposta = Get-ApostaMegaSena -numero $numeros |Select-Object -ExpandProperty Aposta
         Write-Host $aposta -NoNewline
         Write-Host
     }  
     Write-Host ''
     Write-Host ''
     Pause
-
 }
 
-function Invoke-SubMenuLotofacil {
+$showWindowLotofacil = {
     Clear-Host
     Write-Host '==================== Lotofácil ===================' -ForegroundColor Green
     Write-Host ''
@@ -89,7 +92,7 @@ function Invoke-SubMenuLotofacil {
 
     for ($i = 0;$i -lt $jogos;$i++){
        
-        $aposta = Get-ApostaLotofacil -numeros $numeros |Select-Object -ExpandProperty Aposta
+        $aposta = Get-ApostaLotofacil -numero $numeros |Select-Object -ExpandProperty Aposta
         Write-Host $aposta -NoNewline
         Write-Host
     }  
@@ -97,6 +100,34 @@ function Invoke-SubMenuLotofacil {
     Write-Host ''
     Pause
 
+}
+
+$showWindowQuina = {
+    Clear-Host
+Write-Host '==================== QUINA ===================' -ForegroundColor Green
+Write-Host ''
+Write-Host ''
+Start-Sleep -Seconds 2
+[int]$numeros = Read-Host "Digite a quantidade de números da aposta 5 até 10"
+[int]$jogos = Read-Host "Digite a quantidade de jogos 1 até 10"
+Write-Host ''
+Write-Host ''
+
+if($jogos -gt 1){
+    Write-Host 'Apostas Geradas :'
+} Else {
+    Write-Host 'Aposta Gerada :'
+}
+
+for ($i = 0;$i -lt $jogos;$i++){
+   
+    $aposta = Get-ApostaQuina -numero $numeros |Select-Object -ExpandProperty Aposta
+    Write-Host $aposta -NoNewline
+    Write-Host
+}  
+Write-Host ''
+Write-Host ''
+Pause
 }
 Invoke-Menu
 
---
+++ ShowWindowQuina.ps1
@@ -0,0 +1,25 @@
+﻿Clear-Host
+Write-Host '==================== QUINA ===================' -ForegroundColor Green
+Write-Host ''
+Write-Host ''
+Start-Sleep -Seconds 2
+[int]$numeros = Read-Host 'Digite a quantidade de números da aposta (5 até 10)'
+[int]$jogos = Read-Host "Digite a quantidade de jogos (1 até 10)"
+Write-Host ''
+Write-Host ''
+
+if($jogos -gt 1){
+    Write-Host 'Apostas Geradas :'
+} Else {
+    Write-Host 'Aposta Gerada :'
+}
+
+for ($i = 0;$i -lt $jogos;$i++){
+   
+    $aposta = Get-ApostaQuina -numero $numeros |Select-Object -ExpandProperty Aposta
+    Write-Host $aposta -NoNewline
+    Write-Host
+}  
+Write-Host ''
+Write-Host ''
+Pause
