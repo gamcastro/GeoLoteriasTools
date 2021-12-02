d64face05c64b0300bc8cc0c599c7d95ea6b4d0d
George Castro <gamcastro14@gmail.com>
2021-12-02 10:22:08 -0300

    Refatorado codigo.

--- MenuLoterias.ps1
+++ MenuLoterias.ps1
@@ -1,7 +1,6 @@
 ﻿function Invoke-Menu{
     Import-Module GeoHomeTools
     Set-ConsoleFont "Lucida Console" -Size 20
-    Start-Sleep -Seconds 2
     Resize-Console -Maximize
     Clear-Host
   
@@ -31,6 +30,7 @@ for ($i=0;$i -lt $menuwidth;$i++){
 }
 
 Write-Host ($title.PadLeft($pad) + $dataAtual.PadLeft(50)) -ForegroundColor Yellow
+
 for ($i=0;$i -lt $menuwidth;$i++){
     Write-Host "=" -NoNewline
 }
@@ -50,9 +50,9 @@ if((1..5) -notcontains $r){
 
 Write-Verbose "Selecionando a opção"
 switch ($r) {
-    1 { Start-Process powershell -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowMegaSena) -Wait }
-    2 {Start-Process powershell -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowLotofacil) -Wait}
-    3{Start-Process powershell -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowQuina) -Wait}
+    1 { Start-Process powershell -Wait -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowMegaSena)  }
+    2 {Start-Process powershell -Wait -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowLotofacil) }
+    3{Start-Process powershell  -Wait -ArgumentList ("-ExecutionPolicy ByPass " + $showWindowQuina) }
     5 {
         Clear-Host
         Write-Host "Obrigado" -ForegroundColor Green
@@ -63,12 +63,14 @@ switch ($r) {
 
 $showWindowMegaSena = {
     Import-Module GeoHomeTools
-    Set-ConsoleFont -Name 'Lucida Console' -Size 20
-    Start-Sleep -Seconds 1
-    Set-WindowPosition -X 200 -Y 200 -Width 600 -Height 400
-    Start-Sleep -Seconds 1 | Out-Null
+
+    Set-ConsoleFont -Name 'Lucida Console' -Size 20   
+
+    Set-WindowPosition -X 200 -Y 200 -Width 600 -Height 400 | Out-Null  
+    $host.UI.RawUI.WindowTitle = 'MegaSena'
+    
     Resize-Console -Width 80 -Height 20
-    Start-Sleep -Seconds 1
+    
     Clear-Host
     Write-Host '=================================== MegaSena ===================================' -ForegroundColor Green
     Write-Host ''
@@ -83,25 +85,38 @@ $showWindowMegaSena = {
     } Else {
         Write-Host 'Aposta Gerada :'
     }
-
+    Write-Host ''
     for ($i = 0;$i -lt $jogos;$i++){
        
-        $aposta = Get-ApostaMegaSena -numero $numeros |Select-Object -ExpandProperty Aposta
-        Write-Host $aposta -NoNewline
-        Write-Host
+        $aposta = Get-ApostaMegaSena -numero $numeros |Select-Object -Property Aposta
+        Write-Host "Aposta ($i + 1) : `t" -NoNewline
+        foreach ($item in $aposta.Aposta) {           
+            [string]$formatedItem = '{0:D2}' -f $item
+            Write-Host "$formatedItem ' ' "  -NoNewline
+        }
+       Write-Host ''
     }  
     Write-Host ''
     Write-Host ''
     Pause
-}
+}.GetNewClosure()
 
 $showWindowLotofacil = {
+    Import-Module GeoHomeTools
+
+    Set-ConsoleFont -Name 'Lucida Console' -Size 20   
+
+    Set-WindowPosition -X 100 -Y 200 -Width 600 -Height 400 | Out-Null  
+    $host.UI.RawUI.WindowTitle = 'LotoFacil'
+    
+    Resize-Console -Width 100 -Height 20
+    
     Clear-Host
-    Write-Host '==================== Lotofácil ===================' -ForegroundColor Green
+    Write-Host '============================================ LotoFacil ============================================' -ForegroundColor Green
     Write-Host ''
     Write-Host ''
-    [int]$numeros = Read-Host 'Digite a quantidade de números da aposta (15 até 20)'
-    [int]$jogos = Read-Host "Digite a quantidade de jogos (1 até 10)"
+    [int]$numeros = Read-Host "Digite a quantidade de números da aposta `(15 até 20`)"
+    [int]$jogos = Read-Host "Digite a quantidade de jogos `(1 até 10`)"
     Write-Host ''
     Write-Host ''
 
@@ -110,46 +125,62 @@ $showWindowLotofacil = {
     } Else {
         Write-Host 'Aposta Gerada :'
     }
-
+    Write-Host ''
     for ($i = 0;$i -lt $jogos;$i++){
        
-        $aposta = Get-ApostaLotofacil -numero $numeros |Select-Object -ExpandProperty Aposta
-        Write-Host $aposta -NoNewline
-        Write-Host
+        $aposta = Get-ApostaLotofacil -numero $numeros |Select-Object -Property Aposta
+        Write-Host "Aposta ($i + 1) : `t" -NoNewline
+        foreach ($item in $aposta.Aposta) {           
+            [string]$formatedItem = '{0:D2}' -f $item
+            Write-Host "$formatedItem ' ' "   -NoNewline
+        }
+       Write-Host ''
     }  
     Write-Host ''
     Write-Host ''
     Pause
 
-}
+}.GetNewClosure()
 
 $showWindowQuina = {
+    Import-Module GeoHomeTools
+
+    Set-ConsoleFont -Name 'Lucida Console' -Size 20   
+
+    Set-WindowPosition -X 200 -Y 200 -Width 600 -Height 400 | Out-Null  
+    $host.UI.RawUI.WindowTitle = 'Quina'
+    
+    Resize-Console -Width 80 -Height 20
+    
     Clear-Host
-Write-Host '==================== QUINA ===================' -ForegroundColor Green
-Write-Host ''
-Write-Host ''
-Start-Sleep -Seconds 2
-[int]$numeros = Read-Host "Digite a quantidade de números da aposta 5 até 10"
-[int]$jogos = Read-Host "Digite a quantidade de jogos 1 até 10"
-Write-Host ''
-Write-Host ''
+    Write-Host '================================== Quina ==================================' -ForegroundColor Green
+    Write-Host ''
+    Write-Host ''
+    [int]$numeros = Read-Host "Digite a quantidade de números da aposta `(5 até 10`)"
+    [int]$jogos = Read-Host "Digite a quantidade de jogos `(1 até 10`)"
+    Write-Host ''
+    Write-Host ''
 
-if($jogos -gt 1){
-    Write-Host 'Apostas Geradas :'
-} Else {
-    Write-Host 'Aposta Gerada :'
-}
-
-for ($i = 0;$i -lt $jogos;$i++){
-   
-    $aposta = Get-ApostaQuina -numero $numeros |Select-Object -ExpandProperty Aposta
-    Write-Host $aposta -NoNewline
-    Write-Host
-}  
-Write-Host ''
-Write-Host ''
-Pause
-}
+    if($jogos -gt 1){
+        Write-Host 'Apostas Geradas :'
+    } Else {
+        Write-Host 'Aposta Gerada :'
+    }
+    Write-Host ''
+    for ($i = 0;$i -lt $jogos;$i++){
+       
+        $aposta = Get-ApostaQuina -numero $numeros |Select-Object -Property Aposta
+        Write-Host "Aposta ($i + 1) : `t" -NoNewline
+        foreach ($item in $aposta.Aposta) {           
+            [string]$formatedItem = '{0:D2}' -f $item
+            Write-Host "$formatedItem ' ' "   -NoNewline
+        }
+       Write-Host ''
+    }  
+    Write-Host ''
+    Write-Host ''
+    Pause
+}.GetNewClosure()
 
 Invoke-Menu
 
